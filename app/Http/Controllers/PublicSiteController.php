e5c0057ef54fd9389eb4ae95cc36bc90df6cd7fd
TanguyVaucher <1999tanguy@gmail.com>
2026-05-21 14:37:14 +0200

    public / private / admin example

---
+++ app/Http/Controllers/PublicSiteController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+
+class PublicSiteController extends Controller
+{
+    public function home(): View
+    {
+        return view('app');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\PublicSiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('app');
+Route::get('/', [PublicSiteController::class, 'home'])->name('public.home');
+
+Route::get('/admin/login', [AuthController::class, 'create'])->name('login');
+Route::post('/admin/login', [AuthController::class, 'store'])->name('admin.login.store');
+
+Route::middleware(['auth', 'role:superadmin,admin'])->group(function () {
+    Route::get('/admin', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::post('/admin/logout', [AuthController::class, 'destroy'])->name('admin.logout');
 });
