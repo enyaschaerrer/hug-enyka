20b40641b34eaae612d7d3bf641145192f71baf2
TanguyVaucher <1999tanguy@gmail.com>
2026-05-21 11:39:14 +0200

    auth

---
+++ app/Http/Middleware/EnsureUserHasRole.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserHasRole
+{
+    /**
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle(Request $request, Closure $next, string ...$roles): Response
+    {
+        $user = $request->user();
+
+        abort_if($user === null || ! $user->role->isOneOf($roles), 403);
+
+        return $next($request);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserRole;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +19,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::updateOrCreate([
+            'email' => 'superadmin@example.com',
+        ], [
+            'name' => 'Super Admin',
+            'password' => Hash::make('password'),
+            'role' => UserRole::SuperAdmin,
         ]);
     }
 }
